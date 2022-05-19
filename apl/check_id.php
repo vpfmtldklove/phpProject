6b9f33662c69fde7c3a8c5be93e6c2b1ba5d35ba
s.seon <vpfmtldklove@gmail.com>
2022-05-19 16:57:39 +0900

    check_api

--- apl/check_id.php
+++ apl/check_id.php
@@ -3,12 +3,19 @@ include("../common.php");
 
 $email = $_GET['email'];
 
-$sql = "select * from member 
+$sql = "select email from member 
         where email = '$email'";
 
 $result = $conn -> query($sql);
 
 $data = mysqli_fetch_assoc($result);
 
-echo json_encode($data);
+if($data) {
+    // 중복
+    echo json_encode(array('result' => 'y'));
+} else {
+    // 중복이 아니다
+    echo json_encode(array('result' => 'n'));
+}
+
 ?>
