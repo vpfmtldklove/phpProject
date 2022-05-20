141f0202595773f42a647a2daf3a2e36844f2834
s.seon <vpfmtldklove@gmail.com>
2022-05-20 12:24:02 +0900

    boardwriter

--- write_ok.php
+++ write_ok.php
@@ -1,6 +1,36 @@
 <?php 
-    include("common.php");
 
-    print_r($_POST);
-    
+include('common.php');
+
+$title = $_POST['title'];
+$content = addslashes($_POST['content']);
+$writer = $_SESSION['email'];
+$insertTime = date("Y-m-d- H:i:s");
+// print($insertTime);
+
+$sql = "insert into board set
+        title = '$title',
+        content = '$content',
+        writer = '$writer',
+        insertTime = '$insertTime'
+        ";
+echo $sql;
+$result = $conn -> query($sql);
+
+if($result) {
+    echo "
+    <script>
+        alert('등록성공.');
+        location.href = 'index.php';
+    </script>
+    ";
+} else {
+    echo "
+    <script>
+        alert('글 등록 실패했습니다.');
+        location.back();
+    </script>
+    ";
+}
+
 ?>
